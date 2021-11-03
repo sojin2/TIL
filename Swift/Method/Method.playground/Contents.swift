b63da760e554d1b49b88df320fe46fc58a569eaf
sojin2 <jin980729@gmail.com>
2021-11-04 02:05:30 +0900

    20201103

--- Swift/Method/Method.playground/Contents.swift
+++ Swift/Method/Method.playground/Contents.swift
@@ -93,3 +93,77 @@ class Foo {
 let f = Foo()
 f.fooTypeMethod() // Error
 Foo.fooTypeMethod()
+
+struct Lecture {
+    var title: String
+    var maxStudents: Int = 10
+    var numOfRegistered: Int = 0
+    
+    func remainSets() -> Int {
+        let remainSets = maxStudents - numOfRegistered
+        return remainSets
+    }
+    
+    mutating func register() {
+        numOfRegistered += 1
+    }
+    
+    static let target: String = "Anybody want to learn something"
+    static func 소속학원이름() -> String {
+        return "패캠"
+    }
+}
+
+var lec = Lecture(title: "iOS Basic")
+
+lec.register()
+lec.register()
+lec.register()
+lec.register()
+lec.register()
+
+lec.remainSets()
+Lecture.target
+Lecture.소속학원이름()
+
+struct Math {
+    static func abs(value: Int) -> Int {
+        if value > 0 {
+            return value
+        } else {
+            return -value
+        }
+    }
+}
+
+Math.abs(value: -20)
+
+// 제곱, 반값
+extension Math {
+    static func square(value: Int) -> Int {
+        return value * value
+    }
+    static func half(value: Int) -> Int {
+        return value / 2
+    }
+}
+
+Math.square(value: 3)
+Math.square(value: 10)
+
+
+let value: Int = 3
+
+// 제곱, 반값
+
+extension Int {
+    func square() -> Int {
+        return self * self
+    }
+    func half() -> Int {
+        return self / 2
+    }
+}
+
+value.square()
+value.half()
