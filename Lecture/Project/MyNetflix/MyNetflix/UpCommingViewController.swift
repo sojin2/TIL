20507fd9b2f16d2341d39d16bf3062b5f2d82255
sojin2 <jin980729@gmail.com>
2022-06-13 10:00:21 +0900

    Netflix

---
+++ Lecture/Project/MyNetflix/MyNetflix/UpCommingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UpCommingViewController.swift
+//  MyNetflix
+//
+//  Created by HelloDigital_iOS_Dev on 2022/06/10.
+//  Copyright © 2022 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class UpCommingViewController: UIViewController {
+
+    var awardRecommendListViewController: RecommendListViewController!
+    var hotRecommendListViewController: RecommendListViewController!
+    var myRecommendListViewController: RecommendListViewController!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "award" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            awardRecommendListViewController = destinationVC
+            awardRecommendListViewController.viewModel.updateType(.award)
+            awardRecommendListViewController.viewModel.fetchItems()
+        } else if segue.identifier == "hot" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            hotRecommendListViewController = destinationVC
+            hotRecommendListViewController.viewModel.updateType(.hot)
+            hotRecommendListViewController.viewModel.fetchItems()
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
