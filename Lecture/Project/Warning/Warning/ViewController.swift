3fc5fd446632bc8fce4a33b628634913603dd1a2
sojin2 <jin980729@gmail.com>
2022-06-15 23:54:02 +0900

    warning_push

---
+++ Lecture/Project/Warning/Warning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Warning
+//
+//  Created by 김소진 on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
