58ce851cbe2afba12371ec971da91f3cb0f408ca
sojin22 <107539290+sojin22@users.noreply.github.com>
2022-06-23 18:29:50 +0900

    vac

--- Lecture/Project/VaccineLocation/VaccineLocation/Model/ VaccineModel.swift
+++ Lecture/Project/VaccineLocation/VaccineLocation/Model/ VaccineModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+// 필요한 데이터
+// 센터명(centerName), 건물명(facilityName), 주소(address), 업데이트시간(updateAt), 전화번호(phoneNumber)
+
+
+struct CenterVO: Codable{
+    var address: String?
+    var centerName: String?
+    var facilityName: String?
+    var updateAt: String?
+    var phoneNumber: String?
+}
+
