ae173f66f74f412d78f4e27aea3dd683cc2c59b3
sojin22 <107539290+sojin22@users.noreply.github.com>
2022-07-11 18:23:26 +0900

    RxSwift

---
+++ iOS/RxSwift/RxSwift_In_4_Hours-master/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by sojinKim on 2022/07/11.
+//  Copyright © 2022 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
---
+++ iOS/RxSwift/RxSwift_In_4_Hours-master/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  RxSwift+MVVM
+//
+//  Created by iamchiwon on 05/08/2019.
+//  Copyright © 2019 iamchiwon. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    // MARK: - Life Cycle
+    
+    let menus: [Menu] = [
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김1", price: 100, count: 0)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let identifier = segue.identifier ?? ""
+        if identifier == "OrderViewController",
+            let orderVC = segue.destination as? OrderViewController {
+            // TODO: pass selected menus
+        }
+    }
+
+    func showAlert(_ title: String, _ message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertVC, animated: true, completion: nil)
+    }
+
+    // MARK: - InterfaceBuilder Links
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var itemCountLabel: UILabel!
+    @IBOutlet var totalPrice: UILabel!
+
+    @IBAction func onClear() {
+    }
+
+    @IBAction func onOrder(_ sender: UIButton) {
+        // TODO: no selection
+        // showAlert("Order Fail", "No Orders")
+        performSegue(withIdentifier: "OrderViewController", sender: nil)
+    }
+}
+
+extension MenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menus.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
+
+        
+        let menu = menus[indexPath.row]
+        cell.title.text = menu.name
+        cell.price.text = "\(menu.price)"
+        cell.count.text = "\(menu.count)"
+
+        return cell
+    }
+}
