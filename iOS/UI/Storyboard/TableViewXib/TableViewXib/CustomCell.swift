55af091248e424ba69dec14e0917c58644edc5dd
sojin2 <jin980729@gmail.com>
2022-11-22 23:33:24 +0900

    xib_tableView

---
+++ iOS/UI/Storyboard/TableViewXib/TableViewXib/CustomCell.swift
@@ -0,0 +1,28 @@
+//
+//  CatTableViewCell.swift
+//  TableViewXib
+//
+//  Created by 김소진 on 2022/11/22.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+
+    @IBOutlet weak var catImage: UIImageView!
+    @IBOutlet weak var catLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ iOS/UI/Storyboard/TableViewXib/TableViewXib/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TableViewXib
+//
+//  Created by 김소진 on 2022/11/22.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let nibName = UINib(nibName: "CustomCell", bundle: nil)
+
+        tableView.register(nibName, forCellReuseIdentifier: "CustomCell")
+        
+        // registerXib()
+    }
+
+//    private func registerXib() {
+//
+//        let nibName = UINib(nibName: "CustomCell", bundle: nil)
+//
+//        tableView.register(nibName, forCellReuseIdentifier: "CustomCell")
+//
+//    }
+
+}
+
+// catCell
+// CustomCell
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        return cell
+    }
+
+    
+}
+
